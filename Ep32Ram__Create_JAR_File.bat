b7f1422555ba37bdef82c81f05127ce3292c07aa
MooseValley <Moose_Software@yahoo.com.au>
2021-06-09 22:27:41 +1000

    v0.0004
    v0.0004 - build JAR

---
+++ Ep32Ram__Create_JAR_File.bat
@@ -0,0 +1,41 @@
+REM-----------------------------
+REM Ep32Ram
+REM-----------------------------
+echo off
+cls
+
+REM SET "dirlocation=%JAVA_HOME%\bin\"
+
+
+:STARTCOMPILE
+echo "%dirlocation%"
+del *.class /Q
+echo Create the Manifest file:
+echo Main-Class: Ep32Ram >MANIFEST.MF
+echo .
+
+echo Compile the Java code:
+"%dirlocation%javac.exe" ..\00__common_code\Network.java
+copy ..\00__common_code\*.class .
+"%dirlocation%javac.exe" ..\00__common_code\Moose_Utils.java
+copy ..\00__common_code\*.class .
+
+REM Increment the Build Number
+REM "%dirlocation%java.exe" BuildNumberIncrementer
+
+"%dirlocation%javac.exe" "Ep32Ram.java"
+
+echo .
+echo Build the JAR file:
+REM add the BuildNumber.ini to JAR:
+"%dirlocation%jar.exe" cfm "Ep32Ram.jar" MANIFEST.MF BuildNumber.ini *.class ..\icons\*.gif ..\icons\*.png  "Ep32Ram*.txt"
+
+REM
+del *.class
+del ..\00__common_code\*.class
+REM del *.gif
+
+:END
+echo .
+echo Finished!
+pause
---
+++ Ep32Ram__Run_JAR_and_Show_Console_Window.bat
@@ -0,0 +1,3 @@
+REM "%JAVA_HOME%\bin\java.exe" -jar Ep32Ram.jar
+java.exe -jar Ep32Ram.jar
+pause
